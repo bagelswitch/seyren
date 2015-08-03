bfdab88a0bf4f73be3d91fedfd3c57daae0053c8
pingw33n <pingw33n@emphased.net>
2015-08-03 16:46:58 +0300

    Use template for InfluxDB checker

--- seyren-core/src/main/java/com/seyren/core/service/checker/influxdb/DefaultCheckTemplate.java
+++ seyren-core/src/main/java/com/seyren/core/service/checker/influxdb/DefaultCheckTemplate.java
@@ -1,44 +1,42 @@
-package com.seyren.core.service.checker;
+package com.seyren.core.service.checker.influxdb;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import org.joda.time.Instant;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 import com.google.common.base.Optional;
-import com.seyren.core.domain.Check;
+import com.seyren.core.service.checker.support.CheckTemplateSupport;
 import com.seyren.core.util.influxdb.InfluxDbClient;
 import com.seyren.core.util.influxdb.QueryResponse;
 
-@Named
-public class InfluxDbTargetChecker implements TargetChecker {
-
-    private final InfluxDbClient client;
+class DefaultCheckTemplate extends CheckTemplateSupport {
 
     @Inject
-    public InfluxDbTargetChecker(InfluxDbClient client) {
-        this.client = client;
-    }
+    private InfluxDbClient client;
 
     @Override
-    public boolean canHandle(Check check) {
-        return check.getType() == Check.Type.INFLUX_DB;
-    }
-
-    @Override
-    public Map<String, Optional<BigDecimal>> check(Check check) throws Exception {
-        Map<String, Optional<BigDecimal>> r = new HashMap<String, Optional<BigDecimal>>();
-        QueryResponse qr = client.query(check.getTarget());
+    public Map<String, Optional<BigDecimal>> apply() throws IOException {
+        Map<String, Optional<BigDecimal>> r = new HashMap<>();
+        QueryResponse qr = client.query(prepareQuery(getCheck().getTarget()));
         for (QueryResponse.Result result: qr.getResults()) {
             for (QueryResponse.Series series: result.getSeries()) {
                 r.put(series.getName(), Optional.fromNullable(getLatestValue(series)));
             }
         }
         return r;
+    }
+
+    private String prepareQuery(String q) {
+        DateTimeFormatter f = ISODateTimeFormat.dateTime();
+        return q.replace("${timeRange}",
+                "(time > '" + f.print(getCheckFrom()) + "' AND time <= '" + f.print(getCheckUntil()) + "')");
     }
 
     private BigDecimal getLatestValue(QueryResponse.Series series) {
---
+++ seyren-core/src/main/java/com/seyren/core/service/checker/influxdb/InfluxDbTargetChecker.java
@@ -0,0 +1,23 @@
+package com.seyren.core.service.checker.influxdb;
+
+import java.io.IOException;
+
+import javax.inject.Named;
+
+import com.seyren.core.domain.Check;
+import com.seyren.core.service.checker.support.CheckTemplate;
+import com.seyren.core.service.checker.support.TargetCheckerSupport;
+
+@Named
+public class InfluxDbTargetChecker extends TargetCheckerSupport {
+
+    @Override
+    public boolean canHandle(Check check) {
+        return check.getType() == Check.Type.INFLUX_DB;
+    }
+
+    @Override
+    protected CheckTemplate retrieveTemplate(Check check) throws IOException {
+        return new DefaultCheckTemplate();
+    }
+}
